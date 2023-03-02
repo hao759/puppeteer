1c0483748a3eb8684d3f68e25096df449a831959
hao759 <qhao74154@gmail.com>
2023-03-03 05:54:53 +0700

    screen short

---
+++ index.js
@@ -0,0 +1,13 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  page.setViewport({ width: 1280, height: 720 });
+  await page.goto("https://www.leagueoflegends.com/vi-vn/news/game-updates/", {
+    waitUntil: "networkidle2",
+  });
+  await page.screenshot({ path: "Image" });
+
+  await browser.close();
+})();
