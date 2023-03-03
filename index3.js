f7943c78994895bab928d69e2eea6ab5d3cd5f7b
hao759 <qhao74154@gmail.com>
2023-03-03 07:17:21 +0700

    add to file excel

--- index3.js
+++ index3.js
@@ -1,5 +1,7 @@
 const puppeteer = require("puppeteer");
 const download = require("image-downloader");
+const XLSX = require("xlsx");
+
 const linkPage =
   "https://www.leagueoflegends.com/vi-vn/news/game-updates/patch-12-22-notes/";
 (async () => {
@@ -14,19 +16,30 @@ const linkPage =
     let imgElements = document.querySelectorAll("a.skins.cboxElement > img");
     imgElements = [...imgElements];
     let imgLinks = imgElements.map((i) => i.getAttribute("src"));
+
     return imgLinks;
   });
   console.log(imgLinks);
 
   // Tải các ảnh này về thư mục hiện tại
-  await Promise.all(
-    imgLinks.map((imgUrl) =>
-      download.image({
-        url: imgUrl,
-        dest: __dirname + "/Image/CapNhatLol",
-      })
-    )
-  );
+  // await Promise.all(
+  //   imgLinks.map((imgUrl) =>
+  //     download.image({
+  //       url: imgUrl,
+  //       dest: __dirname + "/Image/CapNhatLol",
+  //     })
+  //   )
+  // );
+
+  // imgLinks = [...imgLinks];
+  const arrayOfArrays = imgLinks.map((link) => [link]);
+  // Convert the array object to a workbook
+  const workbook = XLSX.utils.book_new();
+  const worksheet = XLSX.utils.aoa_to_sheet(arrayOfArrays);
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1"); //Sheet
+
+  // Write the workbook to an Excel file
+  XLSX.writeFile(workbook, "data.xlsx"); //file name
 
   await browser.close();
 })();
